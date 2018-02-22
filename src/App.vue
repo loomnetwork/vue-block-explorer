89f23e796324018b2a9c12686b9c6a9973fcf507
Vadim Macagon <vadim.macagon@gmail.com>
2018-02-22 15:35:42 +0700

    Remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
     <BlockExplorer/>
   </div>
 </template>
@@ -8,25 +7,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 // @ts-ignore: Work around for https://github.com/Toilal/vue-webpack-template/issues/62
-// import HelloWorld from './components/HelloWorld.vue';
 import BlockExplorer from './components/BlockExplorer.vue'
 
 @Component({
   components: {
-    // HelloWorld,
     BlockExplorer
   },
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
