87a4ed248788d31670194b27f259d628d36e4fcf
Hank Yang <yrstyh@gmail.com>
2018-03-06 14:43:25 +0800

    create account info styling

--- src/components/delegatecall/CreateAccountTxPreview.vue
+++ src/components/delegatecall/CreateAccountTxPreview.vue
@@ -3,12 +3,18 @@
     <TxPreviewField class="kind" label="Kind:" :value="kind"/>
     <div class="text-white text-title">Owner</div>
     <div class="ml-3">
-      <TxPreviewField label="Chain ID:" :value="owner.chainId"/>
-      <TxPreviewField  label="App:" :value="owner.app"/>
+      <TxPreviewField class="text-bold" label="Chain ID:" :value="owner.chainId"/>
+      <TxPreviewField class="text-bold" label="App:" :value="owner.app"/>
       <TxPreviewField class="address" label="Address:" :value="owner.address"/>
     </div>
     <TxPreviewField class="author" label="Username:" :value="username"/>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.ml-3{
+  margin-left:0 !important;
+}
+</style>
 
 <script lang="ts" src="./create-account-tx-preview.ts"></script>
