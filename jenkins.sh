95f7b024b52e63ebfe45219614f119ec0b082982
Sebastian Klier <sebastian@sebastianklier.com>
2018-08-07 15:03:20 +0800

    typo

--- jenkins.sh
+++ jenkins.sh
@@ -14,5 +14,6 @@ echo "Using bucket $BUCKET_PATH."
 cd dist
 aws s3 cp --recursive . $BUCKET_PATH --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --cache-control 'max-age=86400'
 if [ "$GIT_BRANCH" = "origin/dc-2" ]
+then
 aws cloudfront create-invalidation --distribution-id E17DBOYVX2V35P --paths /\*
 fi
