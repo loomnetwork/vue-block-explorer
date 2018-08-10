d497790b7f9b7e7366acddf21ef6229e0fc66781
Hank Xiao <yrstyh@gmail.com>
2018-08-10 14:30:55 +0800

    update bn type declare

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    open: process.platform === "darwin",
+    host: "0.0.0.0",
+    port: 3000,
+    https: false,
+    hotOnly: false
+  }
 }
